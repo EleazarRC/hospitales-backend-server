ccf857afcf00cd72876fd6afc10a39a37d7eb5a4
Eleazar Ramos Cortés <sixen25@gmail.com>
2022-02-15 13:38:34 +0100

    fin sección 11

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -20,11 +20,11 @@ const getUsuarios = async(req, res) => {
     const total = await Usuario.count(); */
 
 const [ usuarios, total ] = await Promise.all ([
-        Usuario.find( {}, 'nombre email role google')
+        Usuario.find( {}, 'nombre email role google img')
                 .skip( desde )
                 .limit( 5 ),
         
-        Usuario.count()
+        Usuario.countDocuments()
 
 ]);
 
--- routes/uploads.js
+++ routes/uploads.js
@@ -9,13 +9,14 @@ const expressfileUpload = require('express-fileupload');
 
 const { validarJWT } = require("../middlewares/validar-jwt");
 
-const { fileUpload } = require("../controllers/uploads");
+const { fileUpload, retornaImagen } = require("../controllers/uploads");
 
 const router = Router();
 
 router.use(expressfileUpload());
 
 router.put( '/:tipo/:id', [validarJWT], fileUpload );
+router.get( '/:tipo/:foto' , retornaImagen );
 
 
 
